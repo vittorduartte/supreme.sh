833330f1fb1ef521a0cf53be8e34af98bb232c2c
vittorduartte <mateusriograndense@gmail.com>
2021-10-19 10:53:58 -0300

    Script de instalação do node via NVM.

---
+++ node/install.sh
@@ -0,0 +1,22 @@
+installNvm(){
+    echo "Installing NVM..."
+    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh
+    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
+}
+
+configureNvmToZsh(){
+    echo "Configure NVM To ZSH..."
+    echo 'export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm' >> ~/.zshrc
+    echo "!!! Warning !!!"
+    echo "Execute source: ~./bashrc and source ~/.zshrc"
+}
+
+installNode(){
+    echo "Installing Node v14.18.1..."
+    nmv install v14.18.1
+}
+
+installNvm
+configureNvmToZsh
+installNode
