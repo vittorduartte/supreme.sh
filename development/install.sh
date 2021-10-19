0622690ec9a7025c31075165a7a528e857711d1a
vittorduartte <mateusriograndense@gmail.com>
2021-10-19 10:54:21 -0300

    Script de instalação das dependências de desenvolvimento.

---
+++ development/install.sh
@@ -0,0 +1,15 @@
+installYarn(){
+    npm install -g yarn
+}
+
+installVueCli(){
+    npm install -g @vue/cli
+}
+
+installGulp(){
+    npm install --global gulp-cli
+}
+
+installYarn
+installVueCli
+installGulp
